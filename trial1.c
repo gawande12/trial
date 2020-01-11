0f193826b597dd4a8da0381aec02f00b0c3b7a89
gawande12 <44411124+gawande12@users.noreply.github.com>
2020-01-11 23:48:53 +0530

    Update trial1.c

--- trial1.c
+++ trial1.c
@@ -27,9 +27,26 @@ for(i=0;i<n;i++)
         printf("%d\n",*(p+i));
     }
 
+printf("adding more 4 elements\n");
+
+p=(int*)realloc(p,(n+4)*sizeof(int));
+
+if(p==NULL)
+    {
+        printf("exiting the program\n");
+        exit(1);
+    }
+ 
+ for(i=n;i<n+4;i++)
+    {
+        printf("enter the %d element\n",i+1);
+        scanf("%d",p+i);
+    }
+
+
 printf("total elemnts are %d and are as follows\n",n+4);
 
-for(i=0;i<n;i++)
+for(i=0;i<n+4;i++)
     {
         printf("%d element is %d \n",i+1,*(p+i));
     }
